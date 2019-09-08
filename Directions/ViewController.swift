83777c989b2831ccc80711b52b935ee961f430c2
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-08 20:06:19 +0300

    project init

--- Directions/ViewController.swift
+++ Directions/ViewController.swift
@@ -7,14 +7,83 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var textFieldForAddress: UITextField!
+    @IBOutlet weak var getDirectionsButton: UIButton!
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        map.delegate = self
     }
 
-
+    @IBAction func getDirectionTapped(_ sender: Any) {
+        getAddress()
+    }
+    
+    func getAddress() {
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(textFieldForAddress.text!) { (placemarks, error) in
+            guard let placemarks = placemarks, let location = placemarks.first?.location
+                else {
+                    print("No location found!")
+                    return
+            }
+            print(location)
+            self.mapThis(destinationCord: location.coordinate)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+    
+    func mapThis(destinationCord: CLLocationCoordinate2D) {
+        let sourceCordinate = (locationManager.location?.coordinate)!
+        
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceCordinate)
+        let destPlaceMark = MKPlacemark(coordinate: destinationCord)
+        
+        let sourceItem = MKMapItem(placemark: sourcePlaceMark)
+        let destItem = MKMapItem(placemark: destPlaceMark)
+        
+        let destinationRequest = MKDirections.Request()
+        destinationRequest.source = sourceItem
+        destinationRequest.destination = destItem
+        destinationRequest.transportType = .automobile
+        destinationRequest.requestsAlternateRoutes = true
+        
+        let directions = MKDirections(request: destinationRequest)
+        directions.calculate { (response, error) in
+            guard let response = response else {
+                if error != nil {
+                    print("Something is wrong!")
+                }
+                return
+            }
+            let route = response.routes[0]
+            self.map.addOverlay(route.polyline)
+            self.map.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let render = MKPolylineRenderer(overlay: overlay as! MKPolyline)
+        render.strokeColor = .blue
+        return render
+    }
 }
 
